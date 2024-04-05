26615ac61dcee0f071fd32b071925993e64eb933
CarlaRivera53 <112488820+CarlaRivera53@users.noreply.github.com>
2024-04-05 13:19:28 -0600

    Add files via upload

---
+++ practica03/Mensajes.cs
@@ -0,0 +1,13 @@
+using System;
+using imensajes;
+
+namespace mensajes{
+    public class Mensajes : imensajes {
+        public string saludar(string nombre){
+            return "hola"+nombre;
+        }
+        public string mostrar(string nombre){
+            return "x";
+        }
+    }
+}
